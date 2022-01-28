b1dbcef19ea0cc9657972f4f9b8456cfb94eca67
Hrishabh Gupta <hrishabh.gupta@extramarks.com>
2022-01-28 19:57:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useState ,useEffect} from "react";
+import "./App.css";
+import Editor from "./Editor";
+
+function App() {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+        `
+        <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+        </html>
+        `
+      );
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JAVASCRIPT"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane ">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sendbox="allow-script"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        ></iframe>
+      </div>
+    </>
+  );
+}
+
+export default App;
